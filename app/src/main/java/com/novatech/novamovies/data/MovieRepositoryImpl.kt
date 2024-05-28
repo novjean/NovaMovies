ce3620def8f3eab7515be1600245e0ddf911c785
Novjean J Kannathara <novjeanj@gmail.com>
2024-05-28 13:17:40 +0530

    pull movie from api

---
+++ app/src/main/java/com/novatech/novamovies/data/MovieRepositoryImpl.kt
@@ -0,0 +1,44 @@
+package com.novatech.novamovies.data
+
+import com.novatech.novamovies.data.datasource.MovieCacheDataSource
+import com.novatech.novamovies.data.datasource.MovieLocalDataSource
+import com.novatech.novamovies.data.datasource.MovieRemoteDataSource
+import com.novatech.novamovies.data.domain.repository.MovieRepository
+import com.novatech.novamovies.data.model.Movie
+import java.lang.Exception
+
+class MovieRepositoryImpl(
+    private val movieRemoteDataSource: MovieRemoteDataSource,
+    private val movieLocalDataSource: MovieLocalDataSource,
+    private val movieCacheDataSource: MovieCacheDataSource
+) : MovieRepository {
+
+    override suspend fun getMovies(): List<Movie>? {
+        return getMoviesFromCache()
+    }
+
+    override suspend fun updateMovies(): List<Movie>? {
+        val newListOfMovies = getMoviesFromApi()
+    }
+
+    suspend fun getMoviesFromApi(): List<Movie> {
+        lateinit var movieList:List<Movie>
+
+        try{
+            val response = movieRemoteDataSource.getMovies()
+            val body = response.body()
+
+            if(body!=null){
+                movieList=body.movies
+            }
+
+        } catch (e:Exception){
+
+        }
+        return movieList
+    }
+
+    private fun getMoviesFromCache(): List<Movie>? {
+
+    }
+}
